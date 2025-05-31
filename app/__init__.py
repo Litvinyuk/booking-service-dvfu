459af850e6384d6c4669f26ae5388c153e4a6505
Alex <alexandrvip@icloud.com>
2025-05-31 23:41:40 +1000

    login

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+import os
 
 
 def create_app():
     app = Flask(__name__)
+    app.secret_key = 'key_228'
 
     # Регистрируем Blueprint
     from app.routes import main_bp
--- app/models/database.py
+++ app/models/database.py
@@ -2,7 +2,7 @@ import os
 import sqlite3
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))  # корень проекта
-DB_PATH = os.path.join(BASE_DIR, 'data', 'spaces.db')
+DB_PATH = os.path.join(BASE_DIR,'app', 'data', 'spaces.db')
 
 
 def get_connection():
